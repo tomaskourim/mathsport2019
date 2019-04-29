97b0141ed16bb3a6bb27828c81be53aaf6de930a
Tomáš Kouřim <kourim@outlook.com>
2019-04-29 22:48:16 +0200

    to database

--- database_operations.py
+++ database_operations.py
@@ -45,3 +45,30 @@ def execute_sql(db_path: str, query: str, param: Optional, modifiying: bool = Fa
             conn.close()
 
     return return_value
+
+
+def execute_many_sql(db_path: str, query: str, param: Optional, modifiying: bool = False):
+    conn = None
+    cur = None
+    return_value = None
+    try:
+        conn = sqlite3.connect(db_path)
+        cur = conn.cursor()
+        if param is not None:
+            if isinstance(param, list):
+                cur.executemany(query, param)
+            else:
+                cur.executemany(query, (param,))
+        else:
+            cur.executemany(query)
+        return_value = cur.fetchall()
+        if modifiying:
+            conn.commit()
+    except (Exception, sqlite3.DatabaseError) as error:
+        print("Sql exception:", error)
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+
+    return return_value
--- prepare_db.py
+++ prepare_db.py
@@ -1,27 +1,59 @@
 # This is a support file used only for one time extraction of a database from a larger and more complex database
 
 import argparse
-import csv
 import os
+import re
 
-from database_operations import execute_sql
+from database_operations import execute_sql, create_connection, execute_many_sql
 
 ORIGINAL_DATABASE_PATH = 'C://tennis.sqlite'
 
 
-def export_tournaments(first_year, last_year):
+def get_tournaments(first_year, last_year):
     sql = "select * from turnaje where pohlavi=\'men\' and typ=\'dvouhra\' and rok>=? and rok<=? and (nazev like \
     '%Australian open%' or nazev like '%French open%' or nazev like '%Wimbledon%' or nazev like '%US open%')"
 
     tournaments = execute_sql(ORIGINAL_DATABASE_PATH, sql, [first_year, last_year])
-    with open('tournaments.csv', 'w', encoding="utf-8") as myfile:
-        wr = csv.writer(myfile, lineterminator='\n')
-        for tournament in tournaments:
-            wr.writerow(tournament)
-    return tournaments
+    cleaned_tournaments = []
+    for tournament in tournaments:
+        tour = list(tournament[:len(tournament) - 1])
+        tour[3] = "singles"
+        tour[1] = re.sub('ATP ', '', tour[1])
+        tour[1] = re.sub('\\d*', '', tour[1])
+        tour[1] = tour[1].strip()
+        cleaned_tournaments.append(tour)
+
+    return cleaned_tournaments
 
 
-def export_matches_from_tournament(tournament, first_year, matches_filename):
+def translate_month(text):
+    text = text.replace('Led', 'Jan')
+    text = text.replace('Úno', 'Feb')
+    text = text.replace('Bře', 'Mar')
+    text = text.replace('Dub', 'Apr')
+    text = text.replace('Kvě', 'May')
+    text = text.replace('Čer', 'Jun')
+    text = text.replace('Čvc', 'Jul')
+    text = text.replace('Srp', 'Aug')
+    text = text.replace('Zář', 'Sep')
+    text = text.replace('Říj', 'Oct')
+    text = text.replace('Lis', 'Nov')
+    text = text.replace('Pro', 'Dec')
+    return text
+
+
+def get_set_results(match, mat, cur_index):
+    for i in range(5):
+        if mat[2 * i + cur_index] > mat[2 * i + cur_index + 1]:
+            match.append(mat[2 * i + cur_index + 1])
+        elif mat[2 * i + cur_index] < mat[2 * i + cur_index + 1]:
+            match.append(-mat[2 * i + cur_index])
+        else:
+            match.append(None)
+    return match
+
+
+def get_matches(tournament, first_year):
     # qualification is considered part of the tournament, however, it is played as best-of-three only and thus
     # not interesting for this case
     sql = "select min(utime) from( \
@@ -50,40 +82,180 @@ def export_matches_from_tournament(tournament, first_year, matches_filename):
     if len(matches) != 127:
         print(len(matches), tournament)
 
-    with open(matches_filename, 'a', encoding="utf-8") as myfile:
-        wr = csv.writer(myfile, lineterminator='\n')
-        for match in matches:
-            wr.writerow(match)
+    cleaned_matches = []
+    for mat in matches:
+        match = [mat[0], mat[2], list(mat[6:12])]
+        match = remove_nestings(match, [])
+        match[4] = translate_month(match[4])
+        # only looser's sets
+        cur_index = 12
+        match = get_set_results(match, mat, cur_index)
+        # other result
+        if mat[22] == 'null':
+            match.append(None)
+        else:
+            match.append(mat[22])
+        # only looser's tiebreaks
+        cur_index = 23
+        match = get_set_results(match, mat, cur_index)
 
-    return matches
+        cleaned_matches.append(match)
+
+    return cleaned_matches
 
 
 def get_matchids(matches):
     matchids = []
+    unnested_matches = []
     for val in matches:
         for val2 in val:
             matchids.append(val2[0])
-    return matchids
+            unnested_matches.append(val2)
+    return matchids, unnested_matches
+
+
+# function used for removing nested lists in python.
+def remove_nestings(l, output):
+    for i in l:
+        if type(i) == list:
+            remove_nestings(i, output)
+        else:
+            output.append(i)
+    return output
+
+
+def get_odds(bookmaker, matchids):
+    matchids = [2092, 2093, 38929, 120826, 122029]
+
+    questionmarks = '?' * len(matchids)
+    sql = f"select home.id as id, home.id_matchids as id_match, home.sazkovka as bookmaker, home.usek as match_part, \
+            home.odds_1 as odds_home, away.odds_2 as odds_away, max(home.termin,away.termin) as 'date', \
+            max(home.utime,away.utime) as utime from \
+            (select * from home_away \
+                where \
+                (usek='fulltime' or usek='1.set') and \
+                (aktivni='true' or aktivni='false') and \
+                sazkovka=? and \
+                         id_matchids in ({','.join(questionmarks)}) and \
+                         odds_1 not null \
+            ) home \
+            inner join \
+            (select * from home_away \
+                where \
+                (usek='fulltime' or usek='1.set') and \
+                (aktivni='true' or aktivni='false') and \
+                sazkovka=? and \
+                         id_matchids in ({','.join(questionmarks)}) and \
+                         odds_2 not null \
+            ) away \
+            on home.id_matchids=away.id_matchids and home.usek=away.usek"
+
+    params = remove_nestings([bookmaker, matchids, bookmaker, matchids], [])
+    odds = execute_sql(ORIGINAL_DATABASE_PATH, sql, params)
+
+    cleaned_odds = []
+    for oddrow in odds:
+        odd = list(oddrow)
+        odd[5] = translate_month(odd[5])
+        cleaned_odds.append(odd)
+
+    return cleaned_odds
 
 
 def prepare_database(first_year, last_year, bookmaker):
     # select tournaments, export (10 years, 40 tournaments)
-    tournaments = export_tournaments(first_year, last_year)
+    tournaments = get_tournaments(first_year, last_year)
     matches = []
 
     # iterate over each tournament, select matches (filter out errors), export (40 * 127 = 5 080 matches)
-    matches_filename = 'matches.csv'
-    if os.path.isfile(matches_filename):
-        os.remove(matches_filename)
     for tournament in tournaments:
-        matches.append(export_matches_from_tournament(tournament, first_year, matches_filename))
+        matches.append(get_matches(tournament, first_year))
 
     # get matchids
-    matchids = get_matchids(matches)
+    matchids, matches = get_matchids(matches)
 
     # select odds for each match, export (which odds?)
+    odds = get_odds(bookmaker, matchids)
 
-    # create new DB, import data (rename columns?)
+    # create new DB
+    new_database_path = "mathsport2019.sqlite"
+    if os.path.isfile(new_database_path):
+        os.remove(new_database_path)
+    create_connection(new_database_path)
+
+    sql = "CREATE TABLE `tournaments` ( \
+            `id`	INTEGER NOT NULL UNIQUE, \
+            `name`	TEXT NOT NULL, \
+            `competition_type`	TEXT NOT NULL, \
+            `match_type`	TEXT NOT NULL, \
+            `year`	INTEGER NOT NULL, \
+            `country`	TEXT NOT NULL, \
+            `gender`	TEXT NOT NULL, \
+            `surface`	TEXT, \
+            `prize_money`	INTEGER, \
+            `prize_money_currency`	TEXT, \
+            `tournament_link`	TEXT NOT NULL, \
+            PRIMARY KEY(`id`) \
+        );"
+    execute_sql(new_database_path, sql, None, modifiying=True)
+
+    sql = "CREATE TABLE `matches` ( \
+            `id`	INTEGER NOT NULL UNIQUE, \
+            `id_tournament` INTEGER NOT NULL, \
+            `home`	TEXT NOT NULL, \
+            `away`	TEXT NOT NULL, \
+            `date`	TEXT NOT NULL, \
+            `utime`	INTEGER NOT NULL, \
+            `home_sets`	INTEGER, \
+            `away_sets`	INTEGER, \
+            `set1`	INTEGER, \
+            `set2`	INTEGER, \
+            `set3`	INTEGER, \
+            `set4`	INTEGER, \
+            `set5`	INTEGER, \
+            `other_result`	TEXT, \
+            `tiebreak1`	INTEGER, \
+            `tiebreak2`	INTEGER, \
+            `tiebreak3`	INTEGER, \
+            `tiebreak4`	INTEGER, \
+            `tiebreak5`	INTEGER, \
+            PRIMARY KEY(`id`) \
+            FOREIGN KEY(`id_tournament`) REFERENCES tournaments(`id`) \
+        );"
+
+    execute_sql(new_database_path, sql, None, modifiying=True)
+
+    sql = "CREATE TABLE `home_away` ( \
+            `id`	INTEGER NOT NULL UNIQUE, \
+            `id_match`	INTEGER NOT NULL, \
+            `bookmaker`	TEXT NOT NULL, \
+            `match_part`	TEXT NOT NULL, \
+            `odds_home`	REAL, \
+            `odds_away`	REAL, \
+            `date`	TEXT NOT NULL, \
+            `utime`	INTEGER NOT NULL, \
+            PRIMARY KEY(`id`) \
+            FOREIGN KEY(`id_match`) REFERENCES matches(`id`) \
+        );"
+
+    execute_sql(new_database_path, sql, None, modifiying=True)
+
+    # import data
+    questionmarks = '?' * len(tournaments[0])
+    sql = f"INSERT INTO tournaments ( id,name,competition_type,match_type,year,country,gender,surface,prize_money,\
+            prize_money_currency,tournament_link) VALUES ({','.join(questionmarks)})"
+    execute_many_sql(new_database_path, sql, tournaments, True)
+
+    questionmarks = '?' * len(matches[0])
+    sql = f"INSERT INTO matches (id,id_tournament,home,away,date,utime,home_sets,away_sets,set1,set2,set3,set4,set5,\
+                other_result,tiebreak1,tiebreak2,tiebreak3,tiebreak4,tiebreak5) VALUES ({','.join(questionmarks)})"
+    execute_many_sql(new_database_path, sql, matches, True)
+
+    questionmarks = '?' * len(odds[0])
+    sql = f"INSERT INTO matches (id,id_match,bookmaker,match_part,odds_home,odds_away,date,utime) \
+            VALUES ({','.join(questionmarks)})"
+    execute_many_sql(new_database_path, sql, odds, True)
+
     pass
 
 
