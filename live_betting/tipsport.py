94a0ec49863f168e4328e042bdb52e1871e9aa74
Tomáš Kouřim <kourim@outlook.com>
2019-06-26 23:54:49 +0200

    review suggestions 2

--- live_betting/tipsport.py
+++ live_betting/tipsport.py
@@ -2,7 +2,7 @@ import time
 
 from live_betting.bookmaker import Bookmaker
 from live_betting.config_betting import CREDENTIALS_PATH
-from live_betting.utils import load_fb_credentials, write_id, click_id
+from live_betting.utils import load_credentials, write_id, click_id
 
 
 class Tipsport(Bookmaker):
@@ -10,7 +10,7 @@ class Tipsport(Bookmaker):
         Bookmaker.__init__(self, "https://www.tipsport.cz/")
 
     def login(self):
-        username, password = load_fb_credentials(CREDENTIALS_PATH)
+        username, password = load_credentials(CREDENTIALS_PATH)
         write_id(self._driver, "userNameId", username)
         write_id(self._driver, "passwordId", password)
         click_id(self._driver, "btnLogin")
