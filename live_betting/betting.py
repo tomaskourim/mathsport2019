7b4e165a23e8e97095108c38623890d405529add
Tomáš Kouřim <kourim@outlook.com>
2019-08-25 14:56:15 +0200

    logger finalized?

--- live_betting/betting.py
+++ live_betting/betting.py
@@ -96,35 +96,32 @@ def scan_update(book: Bookmaker):
 if __name__ == '__main__':
 
     # Create a custom logger
-    logger = logging.getLogger(__name__)
-
+    logger = logging.getLogger()
     logger.setLevel('DEBUG')
 
     # Create handlers
     total_handler = logging.FileHandler('logfile_total.log')
     info_handler = logging.FileHandler('logfile_info.log')
+    error_handler = logging.FileHandler('logfile_error.log')
     stdout_handler = logging.StreamHandler()
 
     total_handler.setLevel(logging.DEBUG)
     info_handler.setLevel(logging.INFO)
+    error_handler.setLevel(logging.WARNING)
     stdout_handler.setLevel(logging.WARNING)
 
     # Create formatters and add it to handlers
     logging_format = logging.Formatter('%(asctime)s - %(process)d - %(levelname)s - %(name)s - %(message)s')
     total_handler.setFormatter(logging_format)
     info_handler.setFormatter(logging_format)
+    error_handler.setFormatter(logging_format)
     stdout_handler.setFormatter(logging_format)
 
     # Add handlers to the logger
     logger.addHandler(total_handler)
     logger.addHandler(info_handler)
+    logger.addHandler(error_handler)
     logger.addHandler(stdout_handler)
-
-    logger.debug('This is a debug')
-    logger.info('This is a info')
-    logger.warning('This is a warning')
-    logger.error('This is an error')
-    logger.critical('This is an critical')
 
     # in case it crashed with inplay games
     clear_inplay()
