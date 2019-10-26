0674873ea0b5e5971a6c55de6082034866eae922
Tomáš Kouřim <kourim@outlook.com>
2019-10-26 19:30:44 +0200

    manually updated bets

--- betting_analysis/sql_scripts.sql
+++ betting_analysis/sql_scripts.sql
@@ -73,6 +73,30 @@ FROM matches
 WHERE name = 'US Open' AND sex = 'men' AND type = 'singles' AND result NOTNULL;
 
 
+-- manually updated odds
+INSERT INTO bet_manually_updated
+SELECT *
+FROM bet
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT home, away, bet_type, o.match_part, odd, probability, result, odd1, odd2,
+            CASE WHEN bet_type = 'home' AND odd = odd1 OR bet_type = 'away' AND odd = odd2
+                     THEN TRUE
+                 ELSE FALSE END AS odds_match, b.id, CASE WHEN bet_type = 'home'
+                                                              THEN odd1
+                                                          ELSE odd2 END AS or_odds
+        FROM matches
+                 JOIN tournament t ON matches.tournament_id = t.id
+                 JOIN matches_bookmaker mb ON matches.id = mb.match_id
+                 JOIN bet b ON mb.bookmaker_id = b.bookmaker_id AND mb.match_bookmaker_id = b.match_bookmaker_id
+                 INNER JOIN odds o
+        ON b.bookmaker_id = o.bookmaker_id AND b.match_bookmaker_id = o.match_bookmaker_id
+                AND b.match_part = o.match_part
+        WHERE name = 'US Open' AND sex = 'men' AND type = 'singles'
+        ORDER BY odds_match, start_time_utc, home, o.match_part) AS aa
+    WHERE odds_match IS FALSE);
+
 
 -- Berrettini Matteo	Popyrin Alexei	home	set4	1.53	0.6363185834612604	true	-0.026432567304271626	2019-08-31 21:15:00.000000
 -- original odds (acounted by the algo) 1.58
