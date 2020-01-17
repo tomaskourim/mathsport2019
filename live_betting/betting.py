15fa2c35c48dac9db6234391443c2845d5a81e2e
Tomáš Kouřim <kourim@outlook.com>
2020-01-17 17:09:30 +0100

    better error logging

--- live_betting/betting.py
+++ live_betting/betting.py
@@ -74,7 +74,7 @@ def handle_match(bookmaker_matchid: str, c_lambda: float):
         book.handle_match(bookmaker_matchid, c_lambda)
     except Exception as error:
         logger.exception(f"Top level error while handling match {bookmaker_matchid}. Error: {error}")
-        save_screenshot(book.driver, f"top_match_handling", bookmaker_matchid)
+        save_screenshot(book.driver, f"top_match_handling_{error}", bookmaker_matchid)
     finally:
         remove_inplay(bookmaker_matchid, book.database_id)
         book.close()
