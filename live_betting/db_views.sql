59d124b59d1022390572929212c58606444c50a3
Tomáš Kouřim <kourim@outlook.com>
2021-02-06 14:17:29 +0100

    views to 2021

--- live_betting/db_views.sql
+++ live_betting/db_views.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT home, away, start_time_utc, name
 FROM match_course
          JOIN matches m ON match_course.match_id = m.id
          JOIN tournament t ON m.tournament_id = t.id
-WHERE (name LIKE 'ATP%' or name LIKE 'WTA%') AND utc_time_recorded > '2020-09-22 11:00:00.000000'
+WHERE (name LIKE 'ATP%' or name LIKE 'WTA%') AND utc_time_recorded > '2021-01-01 11:00:00.000000'
 GROUP BY match_id, home, away, start_time_utc, name
 ORDER BY start_time_utc;
 
@@ -11,8 +11,8 @@ ORDER BY start_time_utc;
 SELECT match_bookmaker_id
 FROM (SELECT *
       FROM matches
-      WHERE start_time_utc > '2020-09-22 11:00:00.000000' AND
-          start_time_utc < '2020-09-23 22:00:00.000000') AS matches
+      WHERE start_time_utc > '2021-01-01 11:00:00.000000' AND
+          start_time_utc < '2021-02-06 15:00:00.000000') AS matches
          JOIN matches_bookmaker ON matches.id = match_id
          JOIN
 tournament ON matches.tournament_id = tournament.id
@@ -25,7 +25,7 @@ FROM inplay;
 SELECT home, away, start_time_utc, name, sex, type, surface, year
 FROM matches
          JOIN tournament t ON matches.tournament_id = t.id
-WHERE start_time_utc > '2020-09-22 11:00:00.000000' AND (name LIKE '%ATP%' OR name LIKE '%WTA%')
+WHERE start_time_utc > '2021-01-01 11:00:00.000000' AND (name LIKE '%ATP%' OR name LIKE '%WTA%')
 ORDER BY start_time_utc;
 
 -- bets overview
@@ -37,7 +37,7 @@ FROM (SELECT *, mb.match_bookmaker_id AS book_id
       ON bet.bookmaker_id = mb.bookmaker_id AND bet.match_bookmaker_id = mb.match_bookmaker_id
                JOIN matches m ON mb.match_id = m.id
                JOIN tournament t ON m.tournament_id = t.id) AS r
-WHERE start_time_utc >= '2020-09-22 11:00:00.000000'
+WHERE start_time_utc >= '2021-01-01 11:00:00.000000'
 ORDER BY utc_time_recorded DESC, book_id, match_part;
 
 -- odds
@@ -50,7 +50,7 @@ FROM (
     ON odds.bookmaker_id = mb.bookmaker_id AND odds.match_bookmaker_id = mb.match_bookmaker_id
              JOIN matches m ON mb.match_id = m.id
              JOIN tournament t ON m.tournament_id = t.id) AS r
-WHERE start_time_utc >= '2020-09-22 11:00:00.000000'
+WHERE start_time_utc >= '2021-01-01 11:00:00.000000'
 ORDER BY tournament_name, sex, type, book_id, start_time_utc DESC, match_part;
 
 -- match course
@@ -61,7 +61,7 @@ FROM match_course
          JOIN matches m ON match_course.match_id = m.id
          JOIN matches_bookmaker mb ON m.id = mb.match_id
          JOIN tournament t ON m.tournament_id = t.id
-WHERE start_time_utc > '2020-09-22 11:00:00.000000'
+WHERE start_time_utc > '2021-01-01 11:00:00.000000'
 ORDER BY name, sex, type, match_bookmaker_id, start_time_utc, match_bookmaker_id, set_number;
 
 --incorrect results
@@ -75,7 +75,7 @@ WHERE match_bookmaker_id IN (
         FROM (
             SELECT match_id, result, count(*) AS sets_won
             FROM match_course
-            WHERE utc_time_recorded > '2020-09-22 11:00:00.000000'
+            WHERE utc_time_recorded > '2021-01-01 11:00:00.000000'
             GROUP BY match_id, result) AS gid
         GROUP BY match_id) AS gwin
              JOIN matches ON gwin.match_id = matches.id
