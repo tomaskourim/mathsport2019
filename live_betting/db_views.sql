6b749016235cb9998f9aa7a6a4fce2564e07d9d0
Tomáš Kouřim <kourim@outlook.com>
2021-02-08 12:26:23 +0100

    new date

--- live_betting/db_views.sql
+++ live_betting/db_views.sql
@@ -9,7 +9,7 @@ FROM match_course
          JOIN matches m ON match_course.match_id = m.id
          JOIN tournament t ON m.tournament_id = t.id
 WHERE name IN ('ATP Australian Open', 'ATP US Open', 'ATP French Open', 'ATP Wimbledon') AND
-    utc_time_recorded > '2020-09-28 06:00:00.000000'
+    utc_time_recorded > '2021-02-07 22:00:00.000000'
 GROUP BY match_id, home, away, start_time_utc, name
 ORDER BY start_time_utc;
 
@@ -20,8 +20,8 @@ FROM matches
 WHERE match_bookmaker_id IN (SELECT match_bookmaker_id
                              FROM (SELECT *
                                    FROM matches
-                                   WHERE start_time_utc > '2020-10-06 12:00:00.000000' AND
-                                       start_time_utc < '2020-10-06 15:00:00.000000') AS matches
+                                   WHERE start_time_utc > '2021-02-08 12:00:00.000000' AND
+                                       start_time_utc < '2021-02-08 15:00:00.000000') AS matches
                                       JOIN matches_bookmaker ON matches.id = match_id
                                       JOIN
                              tournament ON matches.tournament_id = tournament.id
@@ -36,7 +36,7 @@ WHERE match_bookmaker_id IN (SELECT match_bookmaker_id
 SELECT home, away, start_time_utc, name, sex, type, surface, year
 FROM matches
          JOIN tournament t ON matches.tournament_id = t.id
-WHERE start_time_utc > '2020-09-28 06:00:00.000000' AND
+WHERE start_time_utc > '2021-02-07 22:00:00.000000' AND
     name IN ('ATP Australian Open', 'ATP US Open', 'ATP French Open', 'ATP Wimbledon') AND sex = 'men' AND
     type = 'singles'
 ORDER BY start_time_utc;
@@ -50,7 +50,7 @@ FROM (SELECT *, mb.match_bookmaker_id AS book_id
       ON bet.bookmaker_id = mb.bookmaker_id AND bet.match_bookmaker_id = mb.match_bookmaker_id
                JOIN matches m ON mb.match_id = m.id
                JOIN tournament t ON m.tournament_id = t.id) AS r
-WHERE start_time_utc >= '2020-09-27 06:00:00.000000'
+WHERE start_time_utc >= '2021-02-07 22:00:00.000000'
 ORDER BY utc_time_recorded DESC, book_id, match_part;
 
 -- odds
@@ -63,7 +63,7 @@ FROM (
     ON odds.bookmaker_id = mb.bookmaker_id AND odds.match_bookmaker_id = mb.match_bookmaker_id
              JOIN matches m ON mb.match_id = m.id
              JOIN tournament t ON m.tournament_id = t.id) AS r
-WHERE start_time_utc >= '2020-09-28 06:00:00.000000'
+WHERE start_time_utc >= '2021-02-07 22:00:00.000000'
 ORDER BY tournament_name, sex, type, book_id, start_time_utc DESC, match_part;
 
 -- match course
@@ -74,7 +74,7 @@ FROM match_course
          JOIN matches m ON match_course.match_id = m.id
          JOIN matches_bookmaker mb ON m.id = mb.match_id
          JOIN tournament t ON m.tournament_id = t.id
-WHERE start_time_utc > '2020-09-28 06:00:00.000000'
+WHERE start_time_utc > '2021-02-07 22:00:00.000000'
 ORDER BY name, sex, type, match_bookmaker_id, start_time_utc, match_bookmaker_id, set_number;
 
 --incorrect results
@@ -88,7 +88,7 @@ WHERE match_bookmaker_id IN (
         FROM (
             SELECT match_id, result, count(*) AS sets_won
             FROM match_course
-            WHERE utc_time_recorded > '2020-09-28 06:00:00.000000'
+            WHERE utc_time_recorded > '2021-02-07 22:00:00.000000'
             GROUP BY match_id, result) AS gid
         GROUP BY match_id) AS gwin
              JOIN matches ON gwin.match_id = matches.id
@@ -121,7 +121,7 @@ FROM (SELECT *,
           JOIN matches m ON match_course.match_id = m.id
 ) AS mc
 ON mc.bookmaker_id = o.bookmaker_id AND mc.match_bookmaker_id = o.match_bookmaker_id AND mc.set_number = o.set_number
-WHERE mc.start_time_utc > '2020-09-28 06:00:00.000000' AND o.id ISNULL;
+WHERE mc.start_time_utc > '2021-02-07 22:00:00.000000' AND o.id ISNULL;
 
 --missing match course
 SELECT match_id, match_bookmaker_id, home, away, start_time_utc
@@ -153,7 +153,7 @@ WHERE match_bookmaker_id IN (
     ) AS mc
     ON mc.bookmaker_id = o.bookmaker_id AND mc.match_bookmaker_id = o.match_bookmaker_id
             AND mc.set_number = o.set_number
-    WHERE start_time_utc > '2020-09-28 06:00:00.000000' AND mc.utc_time_recorded ISNULL EXCEPT
+    WHERE start_time_utc > '2021-02-07 22:00:00.000000' AND mc.utc_time_recorded ISNULL EXCEPT
     SELECT match_bookmaker_id
     FROM inplay)
 ORDER BY match_bookmaker_id;
@@ -173,21 +173,21 @@ ORDER BY book_id, start_time_utc DESC, home;
 -- results and expected results
 SELECT sum(probability) AS expected_wins, sum(CASE WHEN result_corrected IS TRUE THEN 1 ELSE 0 END) AS actual_wins
 FROM bet
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 --------------------------------------------------------------
 --theoretical results (if odds were as algorithms expected)
 -- edge
 SELECT *, probability - 1 / odd AS edge
 FROM bet
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL
 ORDER BY edge DESC;
 
 -- expected money wins and actual wins - naive betting summed
 SELECT sum(probability * (odd - 1) - (1 - probability)) AS expected_win,
     sum(CASE WHEN result_corrected IS TRUE THEN odd - 1 ELSE -1 END) AS win
 FROM bet
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 -- expected money wins and actual wins - probability betting summed
 SELECT sum(probability * (probability * odd - probability) - (1 - probability) * probability) AS expected_win,
@@ -195,7 +195,7 @@ SELECT sum(probability * (probability * odd - probability) - (1 - probability) *
                  THEN probability * odd - probability
              ELSE -probability END) AS win
 FROM bet
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 -- expected money wins and actual wins - 1/odds betting summed
 SELECT sum(probability * (1 - 1 / odd) - (1 - probability) * (1 / odd)) AS expected_win,
@@ -203,7 +203,7 @@ SELECT sum(probability * (1 - 1 / odd) - (1 - probability) * (1 / odd)) AS expec
                  THEN (1 - 1 / odd)
              ELSE (-1 / odd) END) AS win
 FROM bet
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 --actual results (actual bet odds)
 -- edge
@@ -211,7 +211,7 @@ SELECT *, probability - 1 / actual_odd AS edge
 FROM (
     SELECT *, CASE WHEN odd_corrected NOTNULL THEN odd_corrected ELSE odd END AS actual_odd
     FROM bet) AS bet_with_actual_odds
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL
 ORDER BY edge DESC;
 
 -- expected money wins and actual wins - naive betting summed
@@ -220,7 +220,7 @@ SELECT sum(probability * (actual_odd - 1) - (1 - probability)) AS expected_win,
 FROM (
     SELECT *, CASE WHEN odd_corrected NOTNULL THEN odd_corrected ELSE odd END AS actual_odd
     FROM bet) AS bet_with_actual_odds
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 -- expected money wins and actual wins - probability betting summed
 SELECT sum(probability * (probability * actual_odd - probability) - (1 - probability) * probability) AS expected_win,
@@ -230,7 +230,7 @@ SELECT sum(probability * (probability * actual_odd - probability) - (1 - probabi
 FROM (
     SELECT *, CASE WHEN odd_corrected NOTNULL THEN odd_corrected ELSE odd END AS actual_odd
     FROM bet) AS bet_with_actual_odds
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
 
 -- expected money wins and actual wins - 1/odds betting summed
 SELECT sum(probability * (1 - 1 / actual_odd) - (1 - probability) * (1 / actual_odd)) AS expected_win,
@@ -240,4 +240,4 @@ SELECT sum(probability * (1 - 1 / actual_odd) - (1 - probability) * (1 / actual_
 FROM (
     SELECT *, CASE WHEN odd_corrected NOTNULL THEN odd_corrected ELSE odd END AS actual_odd
     FROM bet) AS bet_with_actual_odds
-WHERE utc_time_recorded >= '2020-09-28 06:00:00.000000' AND result_corrected NOTNULL;
+WHERE utc_time_recorded >= '2021-02-07 22:00:00.000000' AND result_corrected NOTNULL;
