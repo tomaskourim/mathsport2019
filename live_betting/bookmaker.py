a580201dd7d59fd55392b51c0c7a9ac6692b59b4
Tomas Kourim <kourim@outlook.com>
2021-02-11 22:24:49 +0100

    chrome options to run on ubuntu

--- live_betting/bookmaker.py
+++ live_betting/bookmaker.py
@@ -12,12 +12,14 @@ class Bookmaker:
         self._home_url = home_url
         self.name = name
         options = Options()
-        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         options.add_argument("start-maximized")
         options.add_argument("disable-infobars")
         options.add_argument("--disable-extensions")
         options.add_argument("window-size=1920,1080")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument('--remote-debugging-port=9222')
         self.driver = webdriver.Chrome(options=options, executable_path=WEBDRIVER_PATH)
         self.driver.maximize_window()
         self.driver.get(self._home_url)
